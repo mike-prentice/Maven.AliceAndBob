18d3f204f00e34a576dc042b6e35e0803cc472f4
Mike <mike@Zipcoders-MacBook-Pro.local>
2022-02-23 21:27:11 -0500

    Initial

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,7 +3,15 @@
  */
 public class Main {
 
-    public static void main(String[] args ){
+    public static void main(String[] args) {
+        Scanner inpt = new Scanner(System.in);
+        System.out.println('Enter a name...');
 
+        String name = inpt.nextLine();
+        if (name == "Alice" || name == 'Bob') {
+            System.out.println('Hello there' + name);
+        } else {
+            System.out.println('Enter another name please...');
+        }
     }
 }
